44e8ded0712560d4aa2be8deed728f4b58bc7207
samurai <none@none>
2007-02-02 16:27:03 +0000

    going home
    --HG--
    branch : ubertk
    extra : convert_revision : svn%3A419d067c-91e1-11de-9a8c-5f207592b8ba/ubertk%4039

---
+++ src/utk_huebox.cc
@@ -0,0 +1,52 @@
+// utk_huebox.cc
+
+#include "utk_huebox.h"
+
+namespace utk {
+
+void HueBox::update()
+{
+	unsigned int *dst = pixels;
+	for (int j=0; j<img_h; j++)
+	{
+		for (int i=0; i<img_w; i++)
+		{
+			if (i == sel_h)
+			{
+				unsigned int line_color = 0xFF000000;
+				if (j % 2)
+				{
+					line_color = 0xFFFFFFFF;
+				}
+
+
+			}
+			float hue = (float) i / (float) (img_w - 1);
+
+		}
+	}
+}
+void HueBox::on_click(int x, int y)
+{
+
+}
+
+HueBox::HueBox(utk::Callback cb = 0) : Image(128, 10, cb)
+{
+	h = sel_h = 0;
+}
+HueBox::~HueBox()
+{
+
+}
+
+float HueBox::get_h() const
+{
+
+}
+void HueBox::set_h(float h)
+{
+
+}
+
+} // end namespace utk
---
+++ src/utk_huebox.h
@@ -0,0 +1,30 @@
+// utk_huebox.h
+
+#ifndef _UBERTK_HUEBOX_H_
+#define _UBERTK_HUEBOX_H_
+
+#include <utk_img.h>
+
+namespace utk {
+
+class HueBox : public Image {
+protected:
+	float h;
+	int sel_h;
+
+	virtual void update();
+
+	virtual void on_click(int x, int y);
+	//virtual void on_motion(int x, int y);
+	//virtual void on_drag(int dx, int dy);
+public:
+	HueBox(utk::Callback cb = 0);
+	virtual ~HueBox();
+
+	float get_h() const;
+	void set_h(float h);
+};
+
+}
+
+#endif // ndef _UBERTK_HUEBOX_H_
