9f92d015591d26961b60aac2e43fb41db988f980
nuclear <none@none>
2007-02-21 16:05:22 +0000

    foo
    --HG--
    branch : ubertk
    extra : convert_revision : svn%3A419d067c-91e1-11de-9a8c-5f207592b8ba/ubertk%4053

---
+++ src/utk_common.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include "utk_common.h"
+
+void utk_error(const char *fmt, ...)
+{
+	va_list arg_list;
+
+	va_start(arg_list, fmt);
+	vfprintf(stderr, fmt, arg_list);
+	va_end(arg_list);
+
+#ifdef NDEBUG
+	exit(EXIT_FAILURE);
+#endif
+}
