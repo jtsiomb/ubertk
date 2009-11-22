e76e3fd051e15814f987209f4207d754665a2e1d
mgeorgoulopoulos <none@none>
2009-11-22 18:53:52 +0000

    Forgot to commit this
    --HG--
    extra : convert_revision : svn%3A419d067c-91e1-11de-9a8c-5f207592b8ba/trunk%40106

---
+++ src/utk_config.h
@@ -0,0 +1,11 @@
+/* utk_config.h
+ * UberTK configuration file
+ */
+
+#ifndef _UBERTK_CONFIG_HEADER_
+#define _UBERTK_CONFIG_HEADER_
+
+/* define this to compile UTK with pcre (regular expressions) support */
+/* #define UBERTK_PCRE */
+
+#endif /* ndef _UBERTK_CONFIG_HEADER_ */
